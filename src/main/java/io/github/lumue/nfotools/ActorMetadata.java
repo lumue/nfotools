4c3f9338086f465daa775d7a21c43d5194dfbf4b
lm <mueller.lutz@gmail.com>
2015-12-06 21:36:20 +0100

    initial

---
+++ src/main/java/io/github/lumue/nfotools/ActorMetadata.java
@@ -0,0 +1,8 @@
+package io.github.lumue.nfotools;
+
+/**
+ * Created by lm on 06.12.15.
+ */
+public class ActorMetadata {
+
+}
---
+++ src/main/java/io/github/lumue/nfotools/FileinfoMetadata.java
@@ -0,0 +1,11 @@
+package io.github.lumue.nfotools;
+
+/**
+ * Created by lm on 06.12.15.
+ */
+public class FileinfoMetadata {
+
+
+
+
+}
